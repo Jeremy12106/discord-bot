4c0d18a05ebb262b22416a7c8f2f90e9aafa9ed2
Jeremy12106 <jeremy12106@gmail.com>
2024-12-26 00:38:11 +0800

    (music) testing

---
+++ cogs/music.py
@@ -0,0 +1,7 @@
+import logging as logger
+from .music.player import YTMusic
+
+async def setup(bot):
+    """Initialize the music cog"""
+    await bot.add_cog(YTMusic(bot))
+    logger.info("YTMusic 功能載入成功！")
