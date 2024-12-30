e1f98aa7a76bba42e3e3073675361727e09b879b
Jeremy12106 <jeremy12106@gmail.com>
2024-12-30 09:52:46 +0800

    #45 優化搜尋

--- cogs/meme.py
+++ cogs/meme.py
@@ -8,27 +8,33 @@ from discord import app_commands
 class MyGo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.image_folder = 'assets/image/mygo'
 
     @app_commands.command(name="mygo", description="畢竟是一輩子的事")
     @app_commands.describe(query="輸入關鍵字")
     async def send_image(self, interaction: discord.Interaction, query: str):
         async with interaction.channel.typing():
-            image_folder = 'assets/image/mygo'
-
-            # 使用 glob 模組來匹配包含 query 的圖片檔案
-            pattern = os.path.join(image_folder, f"*{query}*.jpg")  # 可以匹配包含 query 的圖片名稱
+            pattern = os.path.join(self.image_folder, f"*{query}*.jpg")
             matched_images = glob.glob(pattern)
 
-            # 檢查是否有符合條件的圖片
             if matched_images:
-                # 只發送找到的第一張圖片
                 logger.info(f"[MyGo] 伺服器 ID: {interaction.guild.id}, 使用者名稱: {interaction.user.name}, 使用者輸入: {query}, bot 輸出: {os.path.basename(matched_images[0])}")
                 await interaction.response.send_message(file=discord.File(matched_images[0]))
             else:
-                logger.info(f"[MyGo] 伺服器 ID: {interaction.guild.id}, 使用者名稱: {interaction.user.name}, 使用者輸入: {query}, bot 輸出: 我不知道")
-                pattern = os.path.join(image_folder, f"我不知道.jpg")
-                matched_images = glob.glob(pattern)
-                await interaction.response.send_message(file=discord.File(matched_images[0]))
+                logger.debug(f"[MyGo] 伺服器 ID: {interaction.guild.id}, 使用者名稱: {interaction.user.name}, 使用者輸入: {query}, bot 輸出: 我不知道")
+                unknown_image_path = os.path.join(self.image_folder, "我不知道.jpg")
+                if os.path.exists(unknown_image_path):
+                    await interaction.response.send_message(file=discord.File(unknown_image_path))
+                else:
+                    await interaction.response.send_message("抱歉，找不到相關圖片，也找不到預設的 '我不知道.jpg'")
+
+    @send_image.autocomplete("query")
+    async def query_autocomplete(self, interaction: discord.Interaction, current: str):
+        pattern = os.path.join(self.image_folder, "*.jpg")
+        all_images = glob.glob(pattern)
+        image_names = [os.path.splitext(os.path.basename(img))[0] for img in all_images]
+        matches = [name for name in image_names if current.lower() in name.lower()]
+        return [app_commands.Choice(name=match, value=match) for match in matches[:10]]
 
 async def setup(bot):
     await bot.add_cog(MyGo(bot))
